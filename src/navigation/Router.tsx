9081c4ed99123b7e754c8bc4a8073493a950bd10
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-03-02 18:36:33 +0100

    Cleanup

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -3,7 +3,6 @@ import { Layout } from "../layout/Layout";
 import { ContactPage } from "../pages/contactPage/ContactPage";
 import { MainMedicPage } from "../pages/mainMedicPage/MainMedicPage";
 import { MainRefugeePage } from "../pages/mainRefugeePage/MainRefugeePage";
-// import { MedicalPlacesPage } from "../pages/medicalPlacesPage/MedicalPlaces";
 import { MedicalPlacesNewPage } from "../pages/medicalPlacesPage/MedicalPlacesNewPage";
 
 export const Router = () => {
@@ -13,7 +12,6 @@ export const Router = () => {
         <Routes>
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/medic" element={<MainMedicPage />} />
-          {/* <Route path="/places" element={<MedicalPlacesPage />} /> */}
           <Route path="/places" element={<MedicalPlacesNewPage />} />
           <Route path="/" element={<MainRefugeePage />} />
         </Routes>
