baca6adffc9643621ddeb5087a511dbd1d8e495f
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-03-22 22:56:05 +0100

    Cleanup

--- src/pages/medicalPlacesPage/MedicalPlacesNewPage.tsx
+++ src/pages/medicalPlacesPage/MedicalPlacesNewPage.tsx
@@ -1,27 +1,44 @@
 import { Box } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { useGetInstitutions } from "../../react_query/queries/useGetInstitutions";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Loader } from "../../components/Loader/Loader";
+import { Result } from "../../components/Result/Result";
+import { useGetInstitutionsStateMachine } from "../../hooks/stateMachines/useGetInstitutionsStateMachine";
 import { QueryFilter } from "../../types/queryFilter";
 import { MedicalPlaceCard } from "./components/MedicalPlaceCard/MedicalPlaceCard";
 import { MedicalPlacesFilters } from "./components/MedicalPlacesFilters/MedicalPlacesFilters";
 
+const Refetching = () => {
+  return <Loader />;
+};
+
+const NoData = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Result
+      title={t("institutionsListPage.noData")}
+      type="info"
+      subtitle={t("institutionsListPage.tryDifferentFilters")}
+    />
+  );
+};
+
 export const MedicalPlacesNewPage = () => {
   const [filters, setFilters] = useState<QueryFilter[]>([]);
-  const { data: institutions, refetch: getInstitutions } =
-    useGetInstitutions(filters);
-
-  useEffect(() => {
-    getInstitutions();
-  }, [getInstitutions, filters]);
+  const state = useGetInstitutionsStateMachine(filters);
 
   return (
     <>
       <Box w="100%" maxW="90vw">
         <MedicalPlacesFilters onFiltersUpdate={setFilters} />
       </Box>
-      {(institutions || []).map((i) => (
-        <MedicalPlaceCard key={i.id} institution={i} />
-      ))}
+      {["IDLE", "LOADING"].includes(state.type) ? <Loader /> : null}
+      {state.type === "NO_DATA" ? <NoData /> : null}
+      {state.type === "REFETCHING" ? <Refetching /> : null}
+      {state.type === "DATA_FETCHED"
+        ? state.data.map((i) => <MedicalPlaceCard key={i.id} institution={i} />)
+        : null}
     </>
   );
 };
